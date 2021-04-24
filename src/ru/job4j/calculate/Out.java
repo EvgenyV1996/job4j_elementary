c9f831fab6fb504a39d8a87b06c8dd2ade74f46d
EvgenyV1996 <evgenii1496@mail.ru>
2021-04-25 00:52:17 +0500

    9.1. String [#524749]

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args)
+    {
+        String idea = "I like Java!";
+        int year = 2021;
+
+        idea = idea + " But I am a newbie. ";
+        idea = idea + year;
+
+        System.out.println(idea);
+
+    }
+}
