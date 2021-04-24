90e9c1a4e9aec183669c900bfdc27e839a79ba62
EvgenyV1996 <evgenii1496@mail.ru>
2021-04-24 18:15:39 +0500

    4.1. Аргументы [#524750]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -6,9 +6,15 @@ public class ArgMethod
         System.out.println("Hello, " + name);
     }
 
+    //не знаю насколько правильно. Но перенная age как бы намекает на перегрузку)
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + " age " + age);
+    }
+
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
         ArgMethod.hello(name);
+        ArgMethod.hello(name, age);
     }
 }
